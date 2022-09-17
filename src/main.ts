3c2f95ee531362677f8f5cf940da08cc87337d19
cozyGalvinism <jean@der-capta.in>
2022-09-17 11:24:52 +0200

    Bump version

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const script: Firebot.CustomScript<Params> = {
       name: "Tiltify Integration",
       description: "A Tiltify integration for Firebot",
       author: "cozyGalvinism",
-      version: "1.0.0",
+      version: "1.0.1",
       firebotVersion: "5",
       startupOnly: true
     };
